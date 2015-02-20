6710a30bbc83e95360874b95c57d5a4f9735d448
tamirlev <tamir.levy@intel.com>
2015-02-20 15:55:17 +0200

    On owr way to a wix installer

--- ChromeWindowsApp/Riddler2/Properties/Settings.Designer.cs
+++ ChromeWindowsApp/Riddler2/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HAMENAJES.Properties {
+namespace HAMENAJESET.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
